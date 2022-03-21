8cfba26990349592f28a7c99ad2994155eb9018e
Vl Anh <volocanh102>
2022-03-21 22:36:02 +0700

    add query, mutation, resolver

--- server/resolver/resolver.js
+++ server/resolver/resolver.js
@@ -1,18 +1,27 @@
+const { books, authors } = require('../data/static')
 const resolvers = {
+  // QUERY
   Query: {
-    books: () => [
-      {
-        id: 1,
-        name: 'De men phieu luu ke',
-        genre: 'Adventure'
-      },
-      {
-        id: 2,
-        name: 'Lam giau khong kho',
-        genre: 'Education'
-      }
-    ]
+    books: () => books ,
+    book: (parent, args) => books.find(book => book.id = args.id),
+    authors: () => authors,
+    author: (parent, args) => authors.find(author => author.id = args.id),
+  },
+  Book: {
+    author: (parent, args) => {
+      return authors.find(author => author.id === parent.authorId)
+    }
+  },
+  Author: {
+    books: (parent, args) => {
+      return books.filter(book => book.authorId = parent.id)
+    }
+  },
+  // MUTATION
+  Mutation: {
+    createAuthor: (parent, args) =>
   }
-
 }
+
+
 module.exports = resolvers
